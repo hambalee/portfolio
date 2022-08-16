daf11de52150a4712d9ef73fcd99817be8d9bd0a
hambalee <hambalee@outlook.com>
2022-08-17 00:44:43 +0700

    move file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './Layout';
-import Home from '../pages/Home/Home';
-import Contact from '../pages/Contact/Contact';
-import NoPage from '../pages/NoPage/NoPage';
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import NoPage from './pages/NoPage/NoPage';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,26 +22,6 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,21 +1,10 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <NavBar />
 
       <Outlet />
     </>
