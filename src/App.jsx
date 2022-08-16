f6b5692d0ae7a8d2c5adda9ec1127cb0119b836d
hambalee <hambalee@outlook.com>
2022-08-16 23:29:48 +0700

    move page file to pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Layout';
+import Home from '../pages/Home/Home';
+import Contact from '../pages/Contact/Contact';
+import NoPage from '../pages/NoPage/NoPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +16,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="blogs" element={<Blogs />} />
             <Route path="contact" element={<Contact />} />
             <Route path="*" element={<NoPage />} />
           </Route>
